c65b4fa7f2e99006524f8a6abc8cbc02d982e747
Eric Orozco Viscarra <eorozco-viscarra@csumb.edu>
2020-03-18 09:43:47 -0700

    Angular Material Install

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'PrepNep-Frontend';
+
+  outputSets : String[][] = [
+    //["Level set 1", "Var set 1", "Grid set 1", "Time set 1"]
+  ];
+
+  
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  addOutputSet() {
+    this.outputSets.push(["Mandatory raob", "sonde vars", "1 x 1 regular", "Time Set 1"])
+  }
 }
